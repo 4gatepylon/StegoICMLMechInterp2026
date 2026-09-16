94b666c7fbce461a4da0db10e0027944497a9609
4gate <adrianoh@mit.edu>
2026-09-16 12:45:08 -0700

    make format

--- ciphers/kirchenbauer_et_al/src/train_kl_fineweb.py
+++ ciphers/kirchenbauer_et_al/src/train_kl_fineweb.py
@@ -45,9 +45,7 @@ def main() -> None:
         profile_memory_steps=args.profile_memory_steps,
         train_dataset=dataset.skip(args.validation_samples),
         eval_dataset=validation_dataset,
-        data_collator=partial(
-            prefix_bits_encoding_text_collator, tokenizer=tokenizer, n_bits=args.n_bits, data_length=args.data_length
-        ),
+        data_collator=partial(prefix_bits_encoding_text_collator, tokenizer=tokenizer, n_bits=args.n_bits, data_length=args.data_length),
         processing_class=tokenizer,
         peft_config=LoraConfig(task_type="CAUSAL_LM", r=args.lora_rank, lora_alpha=args.lora_alpha, lora_dropout=args.lora_dropout, target_modules="all-linear"),
         args=training_args,
